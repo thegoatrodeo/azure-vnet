daca497daa1863f902254056e9f56fe49125bebe
danfedick <daniel.fedick@hashicorp.com>
2021-12-10 11:04:36 -0500

    Moving to resources vs module

--- main.tf
+++ main.tf
@@ -1,26 +1,83 @@
 
-resource "azurerm_resource_group" "ztarg" {
+resource "azurerm_resource_group" "resource_group" {
   name     = "zt-azure-resource-group"
   location = "East US"
 }
 
-module "vnet" {
-  source              = "Azure/vnet/azurerm"
-  resource_group_name = azurerm_resource_group.ztarg.name
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
   address_space       = ["10.1.0.0/16"]
-  subnet_prefixes     = ["10.1.1.0/24", "10.1.2.0/24"]
-  subnet_names        = ["zt-azure-subnet-a", "zt-azure-subnet-b"]
-
-  subnet_service_endpoints = {
-    subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
-    subnet3 = ["Microsoft.AzureActiveDirectory"]
-  }
-
-  tags = {
-    Terraform   = "true"
-    Environment = "development"
-  }
-
-  depends_on = [azurerm_resource_group.ztarg]
 }
+
+# The subnet where the Virtual Machine will live
+resource "azurerm_subnet" "subnet_1" {
+  name                 = "subnet_1"
+  resource_group_name  = azurerm_resource_group.resource_group.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.1.1.0/24"
+}
+
+# The subnet where the VPN tunnel will live
+resource "azurerm_subnet" "subnet_gateway" {
+  # The name "GatewaySubnet" is mandatory
+  # Only one "GatewaySubnet" is allowed per vNet
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.resource_group.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.1.2.0/24"
+}
+
+
+resource "azurerm_public_ip" "public_ip_1" {
+  name                = "virtual_network_gateway_public_ip_1"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  # Public IP needs to be dynamic for the Virtual Network Gateway
+  # Keep in mind that the IP address will be "dynamically generated" after
+  # being attached to the Virtual Network Gateway below
+  allocation_method = "Dynamic"
+}
+
+resource "azurerm_public_ip" "public_ip_2" {
+  name                = "virtual_network_gateway_public_ip_2"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  # Public IP needs to be dynamic for the Virtual Network Gateway
+  allocation_method = "Dynamic"
+}
+
+resource "azurerm_virtual_network_gateway" "virtual_network_gateway" {
+  name                = "virtual_network_gateway"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  # Configuration for high availability
+  active_active = true
+  # This might me expensive, check the prices  
+  sku           = "VpnGw1"
+
+  # Configuring the two previously created public IP Addresses
+  ip_configuration {
+    name                          = azurerm_public_ip.public_ip_1.name
+    public_ip_address_id          = azurerm_public_ip.public_ip_1.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.subnet_gateway.id
+  }
+
+  ip_configuration {
+    name                          = azurerm_public_ip.public_ip_2.name
+    public_ip_address_id          = azurerm_public_ip.public_ip_2.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.subnet_gateway.id
+  }
+}
+
 
